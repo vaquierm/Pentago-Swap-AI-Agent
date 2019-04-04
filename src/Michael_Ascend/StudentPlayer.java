b3691d571997195d7884958c8650eea937b2a8f1
vaquierm <michaelvaquier74@gmail.com>
2019-04-04 12:01:01 -0400

    Added Ultimate AI and benchmark player

---
+++ src/Michael_Ascend/StudentPlayer.java
@@ -0,0 +1,38 @@
+package Michael_Ascend;
+
+import boardgame.Move;
+
+import pentago_swap.PentagoPlayer;
+import pentago_swap.PentagoBoardState;
+
+/** A player file submitted by a student. */
+public class StudentPlayer extends PentagoPlayer {
+
+    private boolean firstMove = true;
+
+    /**
+     * You must modify this constructor to return your student number. This is
+     * important, because this is what the code that runs the competition uses to
+     * associate you with your agent. The constructor should do nothing else.
+     */
+    public StudentPlayer() {
+        super("Michael Ascended");
+    }
+
+    /**
+     * This is the primary method that you need to implement. The ``boardState``
+     * object contains the current state of the game, which your agent must use to
+     * make decisions.
+     */
+    public Move chooseMove(PentagoBoardState boardState) {
+        long start = System.currentTimeMillis();
+        // Return your move to be processed by the server.
+        long timeToPlay = 1000;
+        System.out.println("Playing move " + boardState.getTurnNumber());
+        Move move = Agent.findBestMoveMontecarlo(timeToPlay, boardState);
+
+        System.out.println("Move played in " + (System.currentTimeMillis() - start) + "ms");
+
+        return move;
+    }
+}
